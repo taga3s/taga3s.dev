894a081bd685807aed2f22c0ea4a982304419856
taga3s <bepozurapu274@gmail.com>
2024-08-27 22:04:44 +0900

    feat: switch to ssg

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import pages from '@hono/vite-cloudflare-pages'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
+import ssg from '@hono/vite-ssg'
 
 export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), pages()]
+  plugins: [honox({ devServer: { adapter } }), pages(), ssg({ entry: "./app/server.ts" })]
 })
