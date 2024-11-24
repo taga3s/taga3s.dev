10d7c238c422f4ceb173d46b76f4e31fb0e892fe
taga3s <bepozurapu274@gmail.com>
2024-11-24 20:02:17 +0900

    refactor(rehype-momiji): fix types and transformers

--- app/packages/rehype-momiji/buildCodeBlock.ts
+++ app/packages/rehype-momiji/buildCodeBlock.ts
@@ -1,13 +1,9 @@
-import { bundledLanguages, bundledThemes, type BundledTheme } from "shiki";
-import { getHighlighter } from "./highlighter";
+import type { BundledLanguage, HighlighterGeneric, BundledTheme } from "shiki";
 import { toHtml } from "hast-util-to-html";
-import type { Element } from "hast";
 import { COLOR_SHIRONERI, COLOR_HAI } from "./colors";
-import { visit } from "unist-util-visit";
-
-const defaultHighlighter = await getHighlighter({ themes: bundledThemes, langs: bundledLanguages });
 
 const buildCodeBlock = (
+  highlighter: HighlighterGeneric<BundledLanguage, BundledTheme>,
   rawCode: string,
   lang: string,
   theme: BundledTheme,
@@ -15,44 +11,44 @@ const buildCodeBlock = (
   filenameBGColor?: string,
   filenameTextColor?: string,
 ) => {
-  const hast = defaultHighlighter.codeToHast(rawCode, {
-    lang: lang,
-    theme: theme,
-  });
   const filenameColorStyle = `background-color: ${filenameBGColor ?? COLOR_HAI}; color: ${filenameTextColor ?? COLOR_SHIRONERI};`;
 
-  if (filename === "") {
-    return toHtml(hast);
-  }
+  const hast = highlighter.codeToHast(rawCode, {
+    lang: lang,
+    theme: theme,
+    transformers: [
+      {
+        pre(this, node) {
+          if (filename === "") return;
 
-  // Add filename to the code block if it exists
-  visit(hast, "element", (node: Element) => {
-    if (
-      node.tagName === "pre" &&
-      node.properties.class &&
-      typeof node.properties.class === "string" &&
-      node.properties.class.includes("shiki")
-    ) {
-      node.properties = {
-        class: node.properties.class,
-        style: `${node.properties.style}; padding-top: 48px;`,
-        tabindex: node.properties.tabindex,
-      };
+          if (
+            node.properties.class &&
+            typeof node.properties.class === "string" &&
+            node.properties.class.includes("shiki")
+          ) {
+            node.properties = {
+              class: node.properties.class,
+              style: `${node.properties.style}; padding-top: 48px;`,
+              tabindex: node.properties.tabindex,
+            };
 
-      node.children.unshift({
-        type: "element",
-        tagName: "div",
-        properties: {
-          style: `position: absolute; top: 0; left: 16px; width: fit-content; margin-bottom: 16px; padding: 4px 8px; font-size: 14px; border-radius: 0 0 4px 4px; ${filenameColorStyle}`,
+            node.children.unshift({
+              type: "element",
+              tagName: "div",
+              properties: {
+                style: `position: absolute; top: 0; left: 16px; width: fit-content; margin-bottom: 16px; padding: 4px 8px; font-size: 14px; border-radius: 0 0 4px 4px; ${filenameColorStyle}`,
+              },
+              children: [
+                {
+                  type: "text",
+                  value: filename,
+                },
+              ],
+            });
+          }
         },
-        children: [
-          {
-            type: "text",
-            value: filename,
-          },
-        ],
-      });
-    }
+      },
+    ],
   });
 
   return toHtml(hast);
