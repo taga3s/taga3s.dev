68189dd146d0a9b169047ead4932836f4a2ddc11
taga3s <bepozurapu274@gmail.com>
2025-10-18 19:27:05 +0900

    Remove HistoryEducationalBackground component and related data

--- apps/website/app/routes/history.tsx
+++ apps/website/app/routes/history.tsx
@@ -1,17 +1,14 @@
 import { createRoute } from "honox/factory";
 import { presenter } from "../components/Presenter.css";
 import { HistoryWorkHistory } from "../components/history/HistoryWorkHistory";
-import { HistoryEducationalBackground } from "../components/history/HistoryEducationalBackground";
 import { HistoryCertification } from "../components/history/HistoryCertification";
 import workHistory from "../data/workHistory/work_history.json" with { type: "json" };
-import educationalBackground from "../data/educationalBackground/educational_background.json" with { type: "json" };
 import certification from "../data/certification/certification.json" with { type: "json" };
 
 export default createRoute(async (c) => {
   return c.render(
     <div class={presenter}>
       <HistoryWorkHistory workHistory={workHistory.content} />
-      <HistoryEducationalBackground educationalBackground={educationalBackground.content} />
       <HistoryCertification certification={certification.content} />
     </div>,
     {
