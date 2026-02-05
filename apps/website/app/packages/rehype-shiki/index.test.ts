9a1f676c5f9ad1e45b90f65604985a72b5104b66
taga3s <bepozurapu274@gmail.com>
2026-02-06 00:36:41 +0900

    fix

--- apps/website/app/packages/rehype-shiki/index.test.ts
+++ apps/website/app/packages/rehype-shiki/index.test.ts
@@ -1,9 +1,9 @@
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import remarkBreaks from "remark-breaks";
-import { rehypeShikiFromHighlighter, highlighter, transformTitle } from "../rehype-shiki";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+import { highlighter, rehypeShikiFromHighlighter, transformTitle } from "../rehype-shiki";
 
 const MOCK_MARKDOWN = "```ts\nconsole.log('Hello, World!')\n```";
 const EXPECTED_HTML =
@@ -11,7 +11,7 @@ const EXPECTED_HTML =
 
 const MOCK_MARKDOWN_WITH_TITLE = "```ts title=\"sample.ts\"\nconsole.log('Hello, World!')\n```";
 const EXPECTED_HTML_WITH_TITLE =
-  '<pre class="shiki github-dark-default" style="background-color:#0d1117;color:#e6edf3;position:relative;padding-top:36px;" tabindex="0"><div style="position: absolute; top: 0; left: 16px; width: fit-content; margin-bottom: 16px; padding: 4px 8px; font-size: 12px; background: #577C8A">sample.ts</div><code><span class="line"><span style="color:#E6EDF3">console.</span><span style="color:#D2A8FF">log</span><span style="color:#E6EDF3">(</span><span style="color:#A5D6FF">\'Hello, World!\'</span><span style="color:#E6EDF3">)</span></span></code></pre>';
+  '<pre class="shiki github-dark-default" style="background-color:#0d1117;color:#e6edf3;position:relative;padding-top:0px;" tabindex="0"><div style="position: sticky; top: 0; left: 0; width: fit-content; margin-bottom: 16px; padding: 4px 8px; font-size: 12px; background: #577C8A">sample.ts</div><code><span class="line"><span style="color:#E6EDF3">console.</span><span style="color:#D2A8FF">log</span><span style="color:#E6EDF3">(</span><span style="color:#A5D6FF">\'Hello, World!\'</span><span style="color:#E6EDF3">)</span></span></code></pre>';
 
 describe("rehype-shiki", () => {
   it("test", async () => {
