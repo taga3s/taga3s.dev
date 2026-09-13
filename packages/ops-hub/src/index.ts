3454d5156cea77553eea7b587762adfab27dcd56
taga3s <107479598+taga3s@users.noreply.github.com>
2026-09-13 23:29:38 +0900

    feat(discord): Implement message sending functionality and add secrets configuration

--- packages/ops-hub/src/index.ts
+++ packages/ops-hub/src/index.ts
@@ -1,11 +1,37 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const sendMessage = async (message: string, discordToken: string, discordChannelId: string) => {
+  try {
+    const loginStatus = await client.login(discordToken);
+    console.log(loginStatus);
+
+    await new Promise((resolve) => client.once("ready", resolve));
+
+    const channel = await client.channels.fetch(discordChannelId);
+    if (channel && channel.isTextBased() && channel.isSendable()) {
+      await channel.send(`test: ${message}`);
+    }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.destroy();
+  }
+};
+
 interface Env {
-  // Add your bindings here
+  DISCORD_TOKEN: SecretsStoreSecret;
+  DISCORD_CHANNEL_ID: SecretsStoreSecret;
 }
 
 export default {
-  async queue(batch, env, ctx): Promise<void> {
+  async queue(batch, env, _ctx): Promise<void> {
+    const DISCORD_TOKEN = await env.DISCORD_TOKEN.get();
+    const DISCORD_CHANNEL_ID = await env.DISCORD_CHANNEL_ID.get();
+
     for (const message of batch.messages) {
-      console.log("Received", message.body);
+      sendMessage(JSON.stringify(message.body), DISCORD_TOKEN, DISCORD_CHANNEL_ID);
     }
   },
 } satisfies ExportedHandler<Env>;
