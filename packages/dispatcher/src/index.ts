398deecc2e03aa136ff83dede3bf22f82e78c026
taga3s <107479598+taga3s@users.noreply.github.com>
2026-09-20 16:00:48 +0900

    feat(dispatcher): Refactor dispatchBlogUpdate to use Promise.all for concurrent execution

--- packages/dispatcher/src/index.ts
+++ packages/dispatcher/src/index.ts
@@ -3,15 +3,23 @@ import { sendMessage, toDiscordMessage } from "./messenger";
 import type { Env } from "./types";
 
 const dispatchBlogUpdate = async (env: Env, webhookUrl: string) => {
-  const result = await env.BLOG_ENGINE_OGP.generate();
-  if (result === undefined || result.blogUrls.length === 0) {
-    console.log("No new blog ogp generated.");
-    return;
-  }
+  await Promise.all([
+    (async () => {
+      const result = await env.BLOG_ENGINE_OGP.generate();
+      if (result === undefined || result.blogUrls.length === 0) {
+        console.log("No new blog ogp generated.");
+        return;
+      }
 
-  await sendMessage(webhookUrl, toDiscordMessage(":bell: ブログの OGP が準備できたよ。確認してね。", result.blogUrls));
-
-  await env.BLOG_ENGINE_RSS.generate();
+      await sendMessage(
+        webhookUrl,
+        toDiscordMessage(":bell: ブログの OGP が準備できたよ。確認してね。", result.blogUrls),
+      );
+    })(),
+    (async () => {
+      await env.BLOG_ENGINE_RSS.generate();
+    })(),
+  ]);
 };
 
 export default {
