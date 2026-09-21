c75843ca46ecc3412f6c782c843d17743fb69ac7
taga3s <107479598+taga3s@users.noreply.github.com>
2026-09-21 12:32:38 +0900

    feat(metrics-engine): Add weekly report rpc endpoint

---
+++ packages/metrics-engine/src/index.ts
@@ -0,0 +1,56 @@
+import { WorkerEntrypoint } from "cloudflare:workers";
+import { Env, Undefinable } from "./types";
+import { createWeeklyReport, reportQuery, WeeklyReport } from "./report";
+
+const API_BASE_URL = "https://api.cloudflare.com/client/v4/graphql";
+const ZONE_ID = "73d4589686f1d129ef6b911acc067420";
+
+export class ReporterEntrypoint extends WorkerEntrypoint<Env> {
+  async create(span: { start: string; end: string }): Promise<Undefinable<WeeklyReport>> {
+    try {
+      const apiToken = await this.env.TAGA3S_DEV_WEBSITE_READ_TOKEN.get();
+
+      const res = await fetch(API_BASE_URL, {
+        headers: {
+          Authorization: `Bearer ${apiToken}`,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          query: reportQuery,
+          variables: {
+            zoneTag: ZONE_ID,
+            filter: {
+              datetime_geq: span.start,
+              datetime_lt: span.end,
+              clientRequestHTTPHost: "taga3s.dev",
+              // ref: https://developers.cloudflare.com/logs/reference/clientrequestsource/
+              // requestSource: "eyeball",
+            },
+          },
+        }),
+      });
+
+      const rawData = await res.json();
+
+      return createWeeklyReport(rawData);
+    } catch (error) {
+      console.error("Something went wrong while creating weekly report", error);
+    }
+  }
+}
+
+export default class extends WorkerEntrypoint<Env> {
+  async fetch() {
+    return new Response(JSON.stringify({ health: "ok" }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
+
+// FIXME
+export type { WeeklyReport };
---
+++ packages/metrics-engine/src/types.ts
@@ -0,0 +1,5 @@
+export interface Env {
+  TAGA3S_DEV_WEBSITE_READ_TOKEN: SecretsStoreSecret;
+}
+
+export type Undefinable<T> = T | undefined;
