7a42992bbe266a0b34d986d356869ef309ace4f8
taga3s <107479598+taga3s@users.noreply.github.com>
2026-09-23 17:21:06 +0900

    chore(metrics-engine): Update secrets refered

--- packages/metrics-engine/src/index.ts
+++ packages/metrics-engine/src/index.ts
@@ -3,7 +3,7 @@ import { Undefinable } from "./types";
 import { createWeeklyReport, reportQuery, WeeklyReport } from "./report";
 
 interface Env {
-  TAGA3S_DEV_WEBSITE_READ_TOKEN: SecretsStoreSecret;
+  TAGA3S_DEV_ACCOUNT_ZONE_READ_TOKEN: SecretsStoreSecret;
   ZONE_ID: SecretsStoreSecret;
 }
 
@@ -12,7 +12,7 @@ const API_BASE_URL = "https://api.cloudflare.com/client/v4/graphql";
 export class ReporterEntrypoint extends WorkerEntrypoint<Env> {
   async create(span: { start: string; end: string }): Promise<Undefinable<WeeklyReport>> {
     try {
-      const apiToken = await this.env.TAGA3S_DEV_WEBSITE_READ_TOKEN.get();
+      const apiToken = await this.env.TAGA3S_DEV_ACCOUNT_ZONE_READ_TOKEN.get();
       const zoneId = await this.env.ZONE_ID.get();
 
       const res = await fetch(API_BASE_URL, {
